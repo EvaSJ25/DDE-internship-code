181d84591e53cceae1d8700d6ad997878ecb9608
Eva SJ <ems237@exeter.ac.uk>
2025-08-05 14:16:59 +0100

    Removed infiltrate commands

--- track_curve_new.jl
+++ track_curve_new.jl
@@ -10,17 +10,14 @@ function track_curve_new(userf,y0,ytan; h=1e-5,userdf=x->jacobian_new(userf,x;h=
     en1[n]=1 #assigns nth component of nth basis vector to 1
 
     for k in 2:nmax+1
-        #@infiltrate
         ypred= ylist[k-1] +s.*ytan
 
-        #@infiltrate
         #Below finds solution yk
         fbound = y-> vcat(userf(y), (ytan'*(y-ypred)))   
-        #@infiltrate 
         newt=newton_new(fbound,ypred) #newton outputs
         ylist[k]=newt[1] #assigns solution yk to be equal to 1st output by newton
         converged=newt[2] #assigns converged to be second output of newton
-        #@infiltrate
+        
         if converged==false #if newton hasn't converged it will break the loop
             println("Has not converged") #tells user why the loop was broken (the solution didn't converge)
             break
@@ -28,10 +25,8 @@ function track_curve_new(userf,y0,ytan; h=1e-5,userdf=x->jacobian_new(userf,x;h=
 
         #Below finds new tangent
         A=vcat(userdf(ylist[k]),ytan')
-        #@infiltrate
         z=A\en1 
         ytan=(z/norm(z,Inf))*(z'*ytan) #infinity norm used and new tangent found 
-        #@infiltrate
     end
     return ylist, ytan #returns values along the curve for f and returns final tangent
 end
