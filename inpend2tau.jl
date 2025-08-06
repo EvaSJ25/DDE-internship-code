905bf9c2dda821dc72f2f2dce8d8fc3125708e92
Eva SJ <ems237@exeter.ac.uk>
2025-08-06 16:08:09 +0100

    Added comments for user understanding

--- inpend2tau.jl
+++ inpend2tau.jl
@@ -1,4 +1,9 @@
 function inpend2tau(uvec,pars)
+    ##inputs: 
+    #uvec is the vector of vectors for x(t), x(t-tau_1),..,x(t-tau_nd)
+    #pars are the parameters
+
+    #outputs: tau for 2 delayed inverted pendulum example 
     tau=[pars[3],pars[4]]
     return tau
 end 
