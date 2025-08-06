0c2fc3f431a428106c1ebb6678e25382a3262273
Eva SJ <ems237@exeter.ac.uk>
2025-08-06 16:11:00 +0100

    Added comments for user understanding

--- invpend_1delay.jl
+++ invpend_1delay.jl
@@ -1,4 +1,9 @@
 function invpend_1delay(u::Vector{Vector{Float64}},pars::Vector{Float64})
+    ##inputs:
+    #u are the values of the states (x(t), x(t-τ)) in vector of vector form
+    #pars are the system parameters
+
+    #output: the values of the system for the given u and parameters
     xdot=u[1][2]
     vdot=sin(u[1][1]) - pars[1]*u[2][1]-pars[2]*u[2][2] #a=pars[1], b=pars[2] here
     return [xdot,vdot]
