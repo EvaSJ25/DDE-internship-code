98541adf4d893558e5ddf92eb28717506d7a3445
Eva SJ <ems237@exeter.ac.uk>
2025-08-06 16:23:04 +0100

    Added comments (and formula) for user understanding

--- mackeyglassfunc.jl
+++ mackeyglassfunc.jl
@@ -1,14 +1,13 @@
+#The Mackey-Glass equation:
+#xdot=β(x(t-τ)/(1+x(t-τ)^n))-γx(t)
 function mackeyglassfunc(u,pars)
-    #pars=[beta,gamma,n] (e.g.=[1.0,1,2]) (pars components need to be in Float64 form)
-    #u=[[x(t)],[x(t-tau)]] (components need to in vector (and Float64) form)
-    #@infiltrate
-    u1=convert(Float64,u[1][1])
+    ##inputs:
+    #u=[[x(t)],[x(t-tau)]] (components need to be in Vector{Float64} form)
+    #pars are the parameters = [beta,gamma,n] (e.g.=[1.0,1,2]) (pars components need to be in Float64 form)
+    u1=convert(Float64,u[1][1]) 
     u2=convert(Float64,u[2][1])
-    #xdot=pars[1]*((u2)/(1+u2^Int(pars[3])))-pars[2]*u1
-    #xdot=pars[1]*((u2)/(1+real((u2+0im)^(pars[3]))))-pars[2]*u1
+    
     xdot=pars[1]*((u2)/1+(real((u2+0im)^(pars[3]))))-pars[2]*u1
-
-    #@infiltrate
 
     return [xdot]
 end 
