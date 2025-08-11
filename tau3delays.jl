07d811eb1a03dc925b4b88f6fac654c934a11796
Eva SJ <ems237@exeter.ac.uk>
2025-08-11 12:22:05 +0100

    Added comments for user understanding

--- tau3delays.jl
+++ tau3delays.jl
@@ -1,4 +1,4 @@
-function tau3delays(uvec, pars) #3 delays for a 2 parameter system
+function tau3delays(uvec, pars) #This is a function for 3 delays for a 2 parameter system
     tau1=pars[3]
     tau2=pars[4]
     tau3=pars[5]
