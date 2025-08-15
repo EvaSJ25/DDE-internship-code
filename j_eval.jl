911c4c2eb7e215ff46aec287c998d21a2b4920ef
Eva SJ <ems237@exeter.ac.uk>
2025-08-15 09:57:02 +0100

    j_eval function with generic method for finding the weights wj

---
+++ j_eval.jl
@@ -0,0 +1,58 @@
+function j_eval(ti,te;diff=0)#j_eval(ti,te,n;diff=0)#, wj=[]) #(n) #n not needed as n=length(ti)
+    ##inputs:
+    #ti=the interpolation points/node
+    #te=the points you want to evaluate at
+    #diff= what derivative you want (0=evaluation, 1= 1st derivative,2= 2nd derivative )
+
+    ##ouput:
+    #J=jacobian 
+
+    nint=length(ti) #number of interpolation points/nodes (n+1)???? e.g. nint=3 => (x_0,x_1,x_2) so n=2
+    nnew=length(te)#number of points wanted to be evaluated
+    lx=fill(NaN,nnew)#creates vector for l(x) values
+
+    for i in 1:nnew
+        lx[i]=1
+        for t_i in ti
+            lx[i]*=(te[i]-t_i)
+            #@infiltrate
+        end
+    end 
+
+    #Computing the weights w_j
+    #do i need to find w_j or should i just use Chebyshev points of the second kind?
+    #In the theory j runs from 0 to n for coding we'll run from 1 to n+1 (to not confuse indices)
+    
+    wjvec=fill(NaN,nint)
+    wjvec[1]=1 #w_0^(0)=1
+    wjvec=fill(1.0,nint)
+    for j in 2:nint #same as for j=1 to j=n in barycentric interpolation source
+        for k in 1:j-1 #k in 0:j-1
+            #@infiltrate
+            #need to have old value
+            wjvec[k]=(ti[k]-ti[j])*wjvec[k]
+            #@infiltrate
+        end 
+        for t in ti[1:j-1]
+            wjvec[j]*=(ti[j]-t)
+            @infiltrate
+        end 
+    end 
+    #@infiltrate
+    for j in 1:nint 
+        wjvec[j]=1/wjvec[j]
+    end 
+    @infiltrate
+
+
+    lvals=fill(NaN,nnew,nint) #the values of l_j(x^(i))
+    for i in 1:nnew
+        for j in 1:nint
+            lvals[i,j]=(lx[i]*wjvec[j])/(te[i]-ti[j])
+        end 
+    end 
+
+    #return lx,wjvec,lvals
+    #return lvals
+    return wjvec
+end 
