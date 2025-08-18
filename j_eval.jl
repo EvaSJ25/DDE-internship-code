f6c80a9982f3e74781e3761d05ea30ef127ee420
Eva SJ <ems237@exeter.ac.uk>
2025-08-18 16:43:22 +0100

    Changed how you find l_j(xi) and started to find derivative

--- j_eval.jl
+++ j_eval.jl
@@ -5,29 +5,17 @@ function j_eval(ti,te;diff=0)#j_eval(ti,te,n;diff=0)#, wj=[]) #(n) #n not needed
     #diff= what derivative you want (0=evaluation, 1= 1st derivative,2= 2nd derivative )
     ##CURRENTLY FUNCTION ONLY WORKS FOR DIFF=0!!!!
 
-    ##ouput:
-    #lvals=  #J=jacobian
-    
-    
-    #for val in te if val in ti
-        #fx[]=fvec[]
-    #else 
-        #...
-    #end
-    #end 
+    ##output:
+    #
 
     nint=length(ti) #number of interpolation points/nodes (n+1)???? e.g. nint=3 => (x_0,x_1,x_2) so n=2
     nnew=length(te)#number of points wanted to be evaluated
-    lx=fill(NaN,nnew)#creates vector for l(x) values
+    #lx=fill(NaN,nnew)#creates vector for l(x) values
 
-
-    for i in 1:nnew
-        lx[i]=1
-        for t_i in ti
-            lx[i]*=(te[i]-t_i)
-            #@infiltrate
-        end
-    end 
+    #if diff==0
+    #elseif diff==1
+    #elseif diff==2
+    #end  
 
     #Computing the weights w_j
     #In the theory j runs from 0 to n for coding we'll run from 1 to n+1 (for Julia indices)
@@ -48,14 +36,29 @@ function j_eval(ti,te;diff=0)#j_eval(ti,te,n;diff=0)#, wj=[]) #(n) #n not needed
     end 
 
 
+    #l_j(x)=(wj/(x-x_j))/sum(wk/(x-x_k))
+
+    numer=0.0
+    denom=0.0
     lvals=fill(NaN,nnew,nint) #the values of l_j(x^(i))
     for i in 1:nnew
         for j in 1:nint
-            lvals[i,j]=(lx[i]*wjvec[j])/(te[i]-ti[j])
+            numer=wjvec[j]/(te[i]-ti[j])
+            #@infiltrate
+            for k in 1:nint
+                denom+=(wjvec[k]/(te[i]-ti[k]))
+                #@infiltrate
+            end 
+            #lvals[i,j]=(lx[i]*wjvec[j])/(te[i]-ti[j])
+            @infiltrate
+            lvals[i,j]=numer/denom
+            numer=0.0
+            denom=0.0
         end 
     end 
 
     #return lx,wjvec,lvals
     #return lvals
-    return wjvec
+    #return wjvec
+    return lvals
 end 
