572f46959c9cd95f639cb531ca619b5ff6e36815
Eva SJ <ems237@exeter.ac.uk>
2025-08-19 10:47:53 +0100

    Added differentiation forms to it

--- binter_example.jl
+++ binter_example.jl
@@ -1,4 +1,12 @@
-function binter_example(x)
-    fun=abs.(x)+0.5*x -x.^2
-    return fun
+function binter_example(x;diff=0)
+    if diff==0
+        fun=abs.(x)+0.5*x -x.^2
+        return fun
+    elseif diff==1 
+        fund1=x./abs.(x) -2*x .+ 0.5
+        return fund1
+    elseif diff==2
+        fund2=-2
+        return fund2
+    end 
 end
