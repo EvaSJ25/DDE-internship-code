65cafac3360a9af135476f94159570566c8a4901
Eva SJ <ems237@exeter.ac.uk>
2025-08-26 13:33:51 +0100

    Added further comments for user understanding

--- shared/jacobian.jl
+++ shared/jacobian.jl
@@ -7,15 +7,15 @@ function jacobian(f,x; h=1e-5)
     ##outputs: The Jacobian (matrix) for the system f at the point x0
 
     n=length(x) #length of vector x
-    m=length(f(x)) #length of vector of outputs of f  e.g. if f is vector with 2 fubfuncton, length of f =2
-    J=fill(NaN,m,n) #array for jacobian matrix J is a matrix with m rows and n columns 
-    ej=fill(0.0,n) #defines ej vectors
+    m=length(f(x)) #length of vector of outputs of f  e.g. if f is vector with 2 subfunctions, length of f =2
+    J=fill(NaN,m,n) #array for Jacobian matrix, J is a matrix with m rows and n columns 
+    ej=fill(0.0,n) #defines ej vectors (basis vectors)
     
     for j in 1:n
         ej=fill(0.0,n) #resets ej for each j iteration
-        ej[j]=1 #sets jth component to 1 (all the others are zero)
+        ej[j]=1 #sets jth component to 1 (all the others are zero) - jth basis vector
 
-        J[:,j]=(1/2h)*(f(x+(h*ej)) - f(x-(h*ej))) #differentiates function f with respect to x j-th component using finite difference
+        J[:,j]=(1/2h)*(f(x+(h*ej)) - f(x-(h*ej))) #differentiates function f with respect to x j-th component using the finite difference method
     end
 
     return J
