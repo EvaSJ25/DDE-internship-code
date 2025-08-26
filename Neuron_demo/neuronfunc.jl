4222093a5690122d6135119b167664d28d2ac112
Eva SJ <ems237@exeter.ac.uk>
2025-08-26 14:52:44 +0100

    Adjusted layout for better user understanding

--- Neuron_demo/neuronfunc.jl
+++ Neuron_demo/neuronfunc.jl
@@ -5,6 +5,7 @@ function neuronfunc(xx::Vector{Vector{Float64}},pars::Vector{Float64})
     ##inputs:
     #xx are the states (and delays) xx[1][2]=x2(t), x[2][1]=x1(t-tau1)
     #pars are the parameters =[κ, β, a12, a21,tau1, tau2, taus], where tau1, tau2, taus are the delays
+    
     x1dot=-pars[1]*xx[1][1] + pars[2]*tanh(xx[4][1]) + pars[3]*tanh(xx[3][2])
     x2dot=-pars[1]*xx[1][2] + pars[2]*tanh(xx[4][2]) + pars[4]*tanh(xx[2][1])  
     return [x1dot,x2dot]
