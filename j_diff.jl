e52ea65200ddb7365928d72529cc51ea83673dfa
Eva SJ <ems237@exeter.ac.uk>
2025-08-26 14:35:55 +0100

    Added reference to key source

--- j_diff.jl
+++ j_diff.jl
@@ -21,17 +21,17 @@ function j_diff(tbase)
     end 
 
     for j in 1:nint
-        wjvec[j]=1/wjvec[j] #divides not to avoid extra unnecessary divisions (wj is 1/(product of t_i) for i≠j)
+        wjvec[j]=1/wjvec[j] #divides not to avoid extra unnecessary divisions (wj formula given in Equation (3.2) in (Berrut et al 2004))
     end 
 
     D1=fill(0.0,nint,nint) #creates blank array for 1st-order differentiation matrix
     for i in 1:nint
         for j in 1:nint
             if i!=j
-                D1[i,j]=(wjvec[j])/(wjvec[i]*(tbase[i]-tbase[j])) 
+                D1[i,j]=(wjvec[j])/(wjvec[i]*(tbase[i]-tbase[j])) #Equation (9.4) in (Berrut et al 2004)
             end 
         end
-        D1[i,i]=-sum(D1[i,:]) 
+        D1[i,i]=-sum(D1[i,:]) #Equation (9.5) in (Berrut et al 2004)
     end 
     return D1
     #Note: Formula in above is taken from -> Barycentric Lagrange Interpolation, J-P. Berrut, L.N. Trefethen, SIAM Review, Vol 46, No.3 pp. 501-517, 2004 
