8ce018f49a9852f79ba297302cfa4fc56fa9f485
Eva SJ <ems237@exeter.ac.uk>
2025-08-29 14:14:57 +0100

    Function for delays of neuron example

--- Neuron_demo/neurontau.jl
+++ Neuron_demo/neurontau.jl
@@ -1,8 +1,8 @@
 function neurontau(uvec, pars) #delay functions for the 3 delays in neuron example
     #pars (the parameters) in this case are [κ, β, a12, a21,tau1, tau2, taus]
-    tau1=pars[5]
-    tau2=pars[6]
-    taus=pars[7]
+    tau1=pars[5] #5th given parameter is delay τ_1
+    tau2=pars[6] #6th given parameter is delay τ_2
+    taus=pars[7] #7th given parameter is delay τ_s
     tau=[tau1,tau2,taus] #vector of the 3 delays
     return tau
 end 
