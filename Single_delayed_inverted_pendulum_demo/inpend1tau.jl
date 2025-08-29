8b9eec84d24383416870ae7f2b104a9096761ba0
Eva SJ <ems237@exeter.ac.uk>
2025-08-29 14:17:36 +0100

    Function for delay in single-delayed inverted pendulum example

--- Single_delayed_inverted_pendulum_demo/inpend1tau.jl
+++ Single_delayed_inverted_pendulum_demo/inpend1tau.jl
@@ -1,10 +1,11 @@
 function inpend1tau(uvec,pars) 
     ##inputs: 
     #uvec is the vector of vectors for x(t), x(t-tau_1),..,x(t-tau_nd)
-    #pars are the parameters
+    #pars are the parameters of the system ([a,b,tau])
 
-    #outputs: tau
+    #outputs:
+    #tau is the delay 
     
-    tau=pars[3] #for inverted pendulum example with one (constant) delay, tau is the 3rd parameter [a,b,tau]
+    tau=pars[3] #for inverted pendulum example with one (constant) delay, tau is the 3rd parameter in pars=[a,b,tau]
     return tau
 end 
