9c0700041a92b5c2b036ad60b3416d073fc49bc9
Eva SJ <ems237@exeter.ac.uk>
2025-08-29 13:32:15 +0100

    Finds initial guess for equilibrium point and initialises a function to find equilibria

--- DDETools/shared/create_ststfunc.jl
+++ DDETools/shared/create_ststfunc.jl
@@ -5,17 +5,17 @@ function create_ststfunc(f_DDE, u0, pars,nd; par_indx=[])
     #u0 is the initial guess (for the states x_i's)
     #pars are parameters of the system (including the delays), ensure that p0 - initial guess of the (varying) parameter - is in this vector
     #nd is the number of delays
-    #par_indx is the index of the parameter the user wishes to vary (to use continuation over)
-    #Leave par_indx empty to use the function output in Newton function
+    #par_indx is the index of the parameter the user wishes to vary (to do continuation over)
+    ##Note: Leave par_indx empty to use the function output in Newton function
 
     ##outputs:
     #y0 is a vector containing the initial guess u0 and initial guess of parameter that u0 is an equilibrium/steady-state for
-    #f_equilibrium is a function that is used to find teh equilibrium (finds f([u,u,..,u],pars))
+    #f_equilibrium is a function that is used to find the equilibrium (finds f(u,u,..,u)=0)
 
     n=length(u0) # number of states (x_i's)
     u0vec=[u0 for _ in 1:nd+1] #repeats the initial guess over all (delayed) states
     np=length(par_indx) #number of varied parameters (e.g. 0 for use in newton function, 1 for finding equilibrium)
-    y0=vcat(u0,pars[par_indx]) #n+1 dimensional vector of initial guess for equilibirum information (u0 with the n+1th term being the parameter being varied))
+    y0=vcat(u0,pars[par_indx]) #n+1 dimensional vector of initial guess for equilibirum information (u0 with the n+1th term being the varied parameter)
 
     #This works for use in newton and track_curve
     function f_equilibrium(y) #creates a function that can be used in newton and track_curve functions to find equilibria
