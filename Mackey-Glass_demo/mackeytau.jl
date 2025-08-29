07f34e74c1025f524a5f85e5e792cb115163a0c5
Eva SJ <ems237@exeter.ac.uk>
2025-08-29 14:11:05 +0100

    Function for delay in Mackey-Glass equation

--- Mackey-Glass_demo/mackeytau.jl
+++ Mackey-Glass_demo/mackeytau.jl
@@ -1,4 +1,4 @@
 function mackeytau(uvec,pars) #delay function for Mackey-Glass equation
-    tau=pars[4] #fourth given paramter is the delay of the system
+    tau=pars[4] #fourth given parameter is the delay of the system
     return tau
 end 
